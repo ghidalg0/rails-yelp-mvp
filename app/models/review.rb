34920198060cbf31e9bfeca63765cb3089387d48
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-05-18 15:31:14 +0200

    it works but not really nice to look at

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-
+  RATINGS = [0, 1, 2, 3, 4, 5].freeze
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: 0..5
+  validates :rating, inclusion: { in: RATINGS }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "french"}
+alala = {name: "alala", address: "56A Camden Town, London", category: "french"}
+bonkebab =  {name: "bonkebab", address: "52 avenue de london, Paris 75008", category: "french"}
+bokoboko =  {name: "bokoboko", address: "plaza dela pizza, Florencia", category: "french"}
+
+[dishoom, pizza_east, alala, bonkebab, bokoboko].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
